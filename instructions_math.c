81f2f564d5fa07242d562ad47147fddb8335bb12
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-13 23:48:39 +0000

    instruc mat

--- instructions_math.c
+++ instructions_math.c
@@ -1,2 +1,18 @@
 #include "monty.h"
+/**
+ * all - prints all the values on the stack from the top
+ * @list: header of the list.
+ * @line: line of the command.
+ * ---------------------
+ */
+void all(stack_t **list, unsigned int line)
+{
+	stack_t *aux = *list;
 
+	
+
+
+
+
+
+}
